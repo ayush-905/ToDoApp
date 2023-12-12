d13c3efee339023e5738f4dee3b4c501ae293243
Ayush <triayush08@gmail.com>
2023-12-12 23:06:58 +0530

    helper file

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -1,1 +1,7 @@
 export const API_URL = 'http://localhost:8080'
+
+export const options = [
+    { value: 'Low', label: 'Low' },
+    { value: 'Medium', label: 'Medium' },
+    { value: 'High', label: 'High' },
+  ]
